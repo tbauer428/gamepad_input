e89802a5c66c7ccf0e155536fc24735afcd40e90
Thomas Bauer <tom.bauer95@gmail.com>
2020-08-09 17:44:19 -0500

    functional prototype

--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,16 @@ use gilrs_core::{EvCode};
 use gilrs::ev::Code;
 use url::Url;
 
-use std::net::TcpListener;
-use std::thread::spawn;
-
-use tungstenite::{accept_hdr, connect, Message, WebSocket};
-use tungstenite::handshake::server::{Request, Response};
+use tungstenite::{connect, Message, WebSocket};
 use std::thread;
 use std::time::Duration;
-use std::borrow::{BorrowMut, Borrow};
 use tungstenite::client::AutoStream;
+use std::thread::JoinHandle;
 
 fn main() {
 
     let (mut socket, response) =
-        connect(Url::parse("ws://192.168.1.56:8080").unwrap()).expect("Can't connect");
+        connect(Url::parse("ws://localhost:8080").unwrap()).expect("Can't connect");
 
     println!("Connected to the server");
     println!("Response HTTP code: {}", response.status());
@@ -25,9 +21,11 @@ fn main() {
     for (ref header, _value) in response.headers() {
         println!("* {}", header);
     }
+    println!("---------------------------");
+
 
     socket
-        .write_message(Message::Text("---gamepad_input connected---".into()))
+        .write_message(Message::Text("---gamepad_input.rs connected---".into()))
         .unwrap();
 
     // socket.close(None);
@@ -35,20 +33,31 @@ fn main() {
 
     let mut gilrs = Gilrs::new().unwrap();
 
+    // start_message_listener(&mut socket);
+
     loop {
+
         while let Some(ev) = gilrs.next_event() {
 
             let info = gilrs.gamepad(ev.id);
 
             match ev.event {
-                EventType::ButtonPressed(Button::South, _) => send_message(ev.event, &mut socket),
-                EventType::ButtonPressed(Button::North, _) => send_message(ev.event, &mut socket),
-                EventType::ButtonPressed(Button::West, _) => send_message(ev.event, &mut socket),
-                EventType::ButtonPressed(Button::East, _) => send_message(ev.event, &mut socket),
-                EventType::ButtonReleased(Button::South, _) => send_message(ev.event, &mut socket),
-                EventType::ButtonReleased(Button::North, _) => send_message(ev.event, &mut socket),
-                EventType::ButtonReleased(Button::West, _) => send_message(ev.event, &mut socket),
-                EventType::ButtonReleased(Button::East, _) => send_message(ev.event, &mut socket),
+                // EventType::ButtonPressed(Button::South, _) =>
+                //     send_message(Option::from(ev.event), &mut socket, None),
+                // EventType::ButtonPressed(Button::North, _) =>
+                //     send_message(Option::from(ev.event), &mut socket, None),
+                EventType::ButtonPressed(Button::West, _) =>
+                    send_message(None, &mut socket, Option::from("ROTATE_RIGHT")),
+                EventType::ButtonPressed(Button::East, _) =>
+                    send_message(None, &mut socket, Option::from("ROTATE_LEFT")),
+                // EventType::ButtonReleased(Button::South, _) =>
+                //     send_message(Option::from(ev.event), &mut socket, None),
+                // EventType::ButtonReleased(Button::North, _) =>
+                //     send_message(Option::from(ev.event), &mut socket, None),
+                // EventType::ButtonReleased(Button::West, _) =>
+                //     send_message(Option::from(ev.event), &mut socket, None),
+                // EventType::ButtonReleased(Button::East, _) =>
+                //     send_message(Option::from(ev.event), &mut socket, None),
                 EventType::Connected => println!("Connected: {}", format!("{:?}", info.id())),
                 EventType::Disconnected => println!("Disconnected: {}", format!("{:?}", info.id())),
                 _ => (),
@@ -61,19 +70,31 @@ fn main() {
 
 }
 
-fn send_message(ev: EventType, sock: &mut WebSocket<AutoStream>) {
-    sock
-        .write_message(Message::Text(format!("{:?}", ev).into()))
-        .unwrap();
+fn send_message(ev: Option<EventType>, sock: &mut WebSocket<AutoStream>, message: Option<&str>) {
+
+    if ev.is_some() {
+        sock
+            .write_message(Message::Text(format!("{:?}", ev.expect("EventType was not valid!")).into()))
+            .unwrap();
+        println!("Sent: {}", format!("{:?}", ev.expect("EventType was not valid!")))
+    } else if ev.is_none() && message.is_some() {
+        sock
+            .write_message(Message::Text(format!("{}", message.expect("Message was not valid!"))))
+            .unwrap();
+        println!("Sent: {}", message.expect("Message was not valid!"))
+    }
+
+
 }
 
+//WIP separate thread for receiving and logging things
 fn start_message_listener(sock: &'static mut WebSocket<AutoStream>) {
     thread::spawn(move || {
         loop {
             let msg = sock.read_message().expect("Error reading message");
             println!("Received: {}", msg);
-            thread::sleep(Duration::from_millis(1));
+            send_message(None, sock, Option::from("I got your message too!"));
+            thread::sleep(Duration::from_millis(3000));
         }
     });
-
 }
