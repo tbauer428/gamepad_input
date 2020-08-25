4526fa93f7b645855982435a1333ac1ece2daf4a
Thomas Bauer <tom.bauer95@gmail.com>
2020-08-24 21:48:46 -0500

    deciding on what part of gamepad to use

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,15 @@
-use gilrs::{Button, EventType, Gilrs, Event};
-use gilrs::ev::Axis::LeftStickX;
-use gilrs_core::{EvCode};
-use gilrs::ev::Code;
+use gilrs::{Button, EventType, Gilrs};
 use url::Url;
 
 use tungstenite::{connect, Message, WebSocket};
 use std::thread;
 use std::time::Duration;
 use tungstenite::client::AutoStream;
-use std::thread::JoinHandle;
 
 fn main() {
 
     let (mut socket, response) =
-        connect(Url::parse("ws://localhost:8080").unwrap()).expect("Can't connect");
+        connect(Url::parse("ws://192.168.1.54:8080").unwrap()).expect("Can't connect");
 
     println!("Connected to the server");
     println!("Response HTTP code: {}", response.status());
@@ -23,17 +19,11 @@ fn main() {
     }
     println!("---------------------------");
 
-
     socket
         .write_message(Message::Text("---gamepad_input.rs connected---".into()))
         .unwrap();
 
-    // socket.close(None);
-
-
     let mut gilrs = Gilrs::new().unwrap();
-
-    // start_message_listener(&mut socket);
 
     loop {
 
@@ -42,27 +32,18 @@ fn main() {
             let info = gilrs.gamepad(ev.id);
 
             match ev.event {
-                // EventType::ButtonPressed(Button::South, _) =>
-                //     send_message(Option::from(ev.event), &mut socket, None),
-                // EventType::ButtonPressed(Button::North, _) =>
-                //     send_message(Option::from(ev.event), &mut socket, None),
+                EventType::ButtonPressed(Button::South, _) =>
+                    send_message(None, &mut socket, Option::from("ROTATE_DOWN")),
+                EventType::ButtonPressed(Button::North, _) =>
+                    send_message(None, &mut socket, Option::from("ROTATE_UP")),
                 EventType::ButtonPressed(Button::West, _) =>
                     send_message(None, &mut socket, Option::from("ROTATE_RIGHT")),
                 EventType::ButtonPressed(Button::East, _) =>
                     send_message(None, &mut socket, Option::from("ROTATE_LEFT")),
-                // EventType::ButtonReleased(Button::South, _) =>
-                //     send_message(Option::from(ev.event), &mut socket, None),
-                // EventType::ButtonReleased(Button::North, _) =>
-                //     send_message(Option::from(ev.event), &mut socket, None),
-                // EventType::ButtonReleased(Button::West, _) =>
-                //     send_message(Option::from(ev.event), &mut socket, None),
-                // EventType::ButtonReleased(Button::East, _) =>
-                //     send_message(Option::from(ev.event), &mut socket, None),
                 EventType::Connected => println!("Connected: {}", format!("{:?}", info.id())),
                 EventType::Disconnected => println!("Disconnected: {}", format!("{:?}", info.id())),
                 _ => (),
             }
-
 
         }
     }
@@ -87,6 +68,7 @@ fn send_message(ev: Option<EventType>, sock: &mut WebSocket<AutoStream>, message
 
 }
 
+#[allow(dead_code)]
 //WIP separate thread for receiving and logging things
 fn start_message_listener(sock: &'static mut WebSocket<AutoStream>) {
     thread::spawn(move || {
